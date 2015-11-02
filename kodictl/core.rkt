484c872af17e234c278596f4ac22ded05660adb4
Rick van de Loo <rickvandeloo@gmail.com>
2015-11-02 22:04:13 +0100

    move rpc functions to rpc.rkt

--- kodictl/core.rkt
+++ kodictl/core.rkt
@@ -1,63 +1,12 @@
 #!/usr/bin/env racket
 #lang racket
-(require racket/cmdline)
 (require racket/pretty)
-(require net/http-client)
 (require json)
 
 (provide kodictl-evaluate-command-or-api-call)
 
-(define dict-get
-  (λ (dict attr)
-     (dict-ref dict (string->symbol attr))))
-
-; take a list and return a list with pairs of each 2 sequential items
-(define split-list-into-list-of-pairs
-  (λ (li [spli (list)] [prev empty])
-     (let ([argslen (length li)])
-          (cond 
-            [(zero? argslen) spli]
-            [(odd? argslen) 
-		  (split-list-into-list-of-pairs 
-		    (cdr li) 
-		    (append spli (list (cons 
-					 (if 
-					   (empty? prev)
-					   (error "Uneven arguments!")
-					   prev)
-					 (car li)))))]
-	    [else (split-list-into-list-of-pairs 
-		    (cdr li) 
-		    spli 
-		    (car li))]))))
-
-; cast to number if string only contains digits, otherwise cast to symbol
-(define cast-string-to-type 
-  (λ (str) 
-     (if 
-       (regexp-match #px"^\\d*$" str)
-       (string->number str) 
-       (string->symbol str))))
-
-; http-rendrecv returns three values, we only care about the port
-; because it contains the response json from the json-rpc API
-(define port-from-http-response
-  (λ (http-response)
-     (caddr (call-with-values http-response list))))
-
-; http post data in the form of a string
-(define post-str-to-host
-  (λ (str #:host host #:port port #:uri uri #:headers headers)
-     (http-sendrecv host uri #:port port #:method "POST"
-                	     #:headers headers #:data str)))
-
-; post string to json-rpc
-(define post-str-to-json-rpc-host
-  (λ (jsonstr #:host host #:port port #:uri uri 
-	      #:headers [headers '("Content-Type: application/json")])
-     (post-str-to-host jsonstr #:host host 
-		       #:port port #:uri uri 
-		       #:headers headers)))
+(require "rpc.rkt")
+(require "utils.rkt")
 
 ; send a string to kodi's json-rpc and return the reponse string
 (define kodi-json-rpc-call-str
@@ -83,12 +32,6 @@
 	     'method method
 	     'params params
 	     'id id)))
-
-(define create-hash-from-arguments
-  (λ (args)
-     (for/hash ([pair (split-list-into-list-of-pairs args)])
-	       (values (string->symbol (car pair)) 
-		       (cdr pair)))))
 
 (define kodi-json-rpc-action
   (λ args
---
+++ kodictl/utils.rkt
@@ -0,0 +1,46 @@
+#!/usr/bin/env racket
+#lang racket/base
+
+(provide cast-string-to-type)
+(provide split-list-into-list-of-pairs)
+(provide dict-get)
+(provide create-hash-from-arguments)
+
+(define dict-get
+  (λ (dict attr)
+     (dict-ref dict (string->symbol attr))))
+
+; take a list and return a list with pairs of each 2 sequential items
+(define split-list-into-list-of-pairs
+  (λ (li [spli (list)] [prev empty])
+     (let ([argslen (length li)])
+          (cond 
+            [(zero? argslen) spli]
+            [(odd? argslen) 
+		  (split-list-into-list-of-pairs 
+		    (cdr li) 
+		    (append spli (list (cons 
+					 (if 
+					   (empty? prev)
+					   (error "Uneven arguments!")
+					   prev)
+					 (car li)))))]
+	    [else (split-list-into-list-of-pairs 
+		    (cdr li) 
+		    spli 
+		    (car li))]))))
+
+; cast to number if string only contains digits, otherwise cast to symbol
+(define cast-string-to-type 
+  (λ (str) 
+     (if 
+       (regexp-match #px"^\\d*$" str)
+       (string->number str) 
+       (string->symbol str))))
+
+(define create-hash-from-arguments
+  (λ (args)
+     (for/hash ([pair (split-list-into-list-of-pairs args)])
+	       (values (string->symbol (car pair)) 
+		       (cdr pair)))))
+
